14d17a211c45153aa9d3056fd2bfafeebfe705db
samxaradzenika <murdoc746@gmail.com>
2023-03-12 04:20:19 +0400

    keys , conditionals , etc.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,34 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
-import './Expenses.css';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
+import ExpensesList from './ExpensesList';
 
 function Expenses(props) {
+   useState(props.items);
+
+   const [filteredYear, setFilteredYear] = useState('2020');
+
+   const filterChangedHandler = (selectedYear) => {
+      setFilteredYear(selectedYear);
+   };
+   const filteredExpenses = props.items.filter((expense) => {
+      return expense.date.getFullYear().toString() === filteredYear;
+   });
+
    return (
       <Card className="expenses">
-               
-         <ExpenseItem
-            title={props.items[0].title}
-            amount={props.items[0].amount}
-            date={props.items[0].date}
+         <ExpensesFilter
+            selected={filteredYear}
+            onChangeFilter={filterChangedHandler}
          />
-         <ExpenseItem
-            title={props.items[1].title}
-            amount={props.items[1].amount}
-            date={props.items[1].date}
-         />
-         <ExpenseItem
-            title={props.items[2].title}
-            amount={props.items[2].amount}
-            date={props.items[2].date}
-         />
-         <ExpenseItem
-            title={props.items[3].title}
-            amount={props.items[3].amount}
-            date={props.items[3].date}
-         />
+         <ExpensesList items={filteredExpenses} />
+
       </Card>
    );
 }
 export default Expenses;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* {filteredExpenses.items.map((expense) => (
+   <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+   />
+))} */}
+{/* <ExpenseItem
+   title={props.items[0].title}
+   amount={props.items[0].amount}
+   date={props.items[0].date}
+/>
+<ExpenseItem
+   title={props.items[1].title}
+   amount={props.items[1].amount}
+   date={props.items[1].date}
+/>
+<ExpenseItem
+   title={props.items[2].title}
+   amount={props.items[2].amount}
+   date={props.items[2].date}
+/>
+<ExpenseItem
+   title={props.items[3].title}
+   amount={props.items[3].amount}
+   date={props.items[3].date}
+/> */}
